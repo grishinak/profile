0e968858e2b3af2fbe65741b015837a85842180e
grishinak <st122261@student.spbu.ru>
2024-04-23 00:59:24 +0300

    renamed variables

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <vector>//??dynamic
 #include <cstdlib>
 #include <chrono>
 
@@ -35,51 +35,53 @@ int main() {
     //     std::cout << std::endl;
     // }
 
-auto start1 = std::chrono::high_resolution_clock::now();
+auto row_start = std::chrono::high_resolution_clock::now();
 
-    int total_sum1 = 0;
+    int row_total_sum = 0;
     // Сумма по строкам
     std::vector<int> row_sum(rows, 0);
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             row_sum[i] += matrix[i][j];
-            total_sum1 += matrix[i][j];
+            row_total_sum += matrix[i][j];
         }
     }
-  auto end1 = std::chrono::high_resolution_clock::now();
-  auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
-  std::cout <<"Сумма по строкам time: " << duration1.count() << std::endl;
+auto row_end = std::chrono::high_resolution_clock::now();
+auto row_duration = std::chrono::duration_cast<std::chrono::milliseconds>(row_end - row_start);
+std::cout <<"Сумма по строкам длительность: " << row_duration.count() << std::endl;
 
 
 
-auto start2 = std::chrono::high_resolution_clock::now();
-    int total_sum2 = 0;
+auto col_start = std::chrono::high_resolution_clock::now();
+
+    int col_total_sum = 0;
     // Сумма по столбцам
     std::vector<int> col_sum(cols, 0);
     for (int j = 0; j < cols; ++j) {
         for (int i = 0; i < rows; ++i) {
             col_sum[j] += matrix[i][j];
-            total_sum2 += matrix[i][j];
+            col_total_sum += matrix[i][j];
         }
     }
 
-  auto end2 = std::chrono::high_resolution_clock::now();
-  auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2);
-  std::cout <<"Сумма по столбцам time: " <<duration2.count() << std::endl;
+auto col_end = std::chrono::high_resolution_clock::now();
+auto col_duration = std::chrono::duration_cast<std::chrono::milliseconds>(col_end - col_start);
+std::cout <<"Сумма по столбцам длительность: " <<col_duration.count() << std::endl;
+
     // Вывод результатов
 
     // std::cout << "Сумма по строкам:\n";
     // for (int i = 0; i < rows; ++i) {
     //     std::cout << "Строка " << i+1 << ": " << row_sum[i] << std::endl;
     // }
-    std::cout << "\n Общая сумма всех элементов по строкам: " << total_sum1 << std::endl;
+    std::cout << "\n Общая сумма всех элементов по строкам: " << row_total_sum << std::endl;
 
 
     // std::cout << "\n Сумма по столбцам:\n";
     // for (int j = 0; j < cols; ++j) {
     //     std::cout << "Столбец " << j+1 << ": " << col_sum[j] << std::endl;
     // }
-    std::cout << "\n Общая сумма всех элементов по столбцам: " << total_sum2 << std::endl;
+    std::cout << "\n Общая сумма всех элементов по столбцам: " << col_total_sum << std::endl;
     
     return 0;
 }
