fe0cfa0eb2a5b3dee66657d0e3e759b8a3045ebe
grishinak <st122261@student.spbu.ru>
2024-04-23 01:10:33 +0300

    functions for main actions

--- README.md
+++ README.md
@@ -11,16 +11,17 @@ valgrind --tool=cachegrind ./main
 
 
 
-==7014== Cachegrind, a cache and branch-prediction profiler
-==7014== Copyright (C) 2002-2017, and GNU GPL'd, by Nicholas Nethercote et al.
-==7014== Using Valgrind-3.21.0 and LibVEX; rerun with -h for copyright info
-==7014== Command: ./main
-==7014== 
-Сумма по строкам time: 334
-Сумма по столбцам time: 334
+==8489== Cachegrind, a cache and branch-prediction profiler
+==8489== Copyright (C) 2002-2017, and GNU GPL'd, by Nicholas Nethercote et al.
+==8489== Using Valgrind-3.21.0 and LibVEX; rerun with -h for copyright info
+==8489== Command: ./main
+==8489== 
+Сумма по строкам. длительность: 338
 
  Общая сумма всех элементов по строкам: 50498583
+Сумма по столбцам. длительность: 343
 
  Общая сумма всех элементов по столбцам: 50498583
-==7014== 
-==7014== I refs:        316,179,887
+==8489== 
+==8489== I refs:        322,184,546
+
--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <vector>//??dynamic
+#include <vector>
 #include <cstdlib>
 #include <chrono>
 
 std::vector<std::vector<int>> createRandomMatrix(int k, int l) {
     std::vector<std::vector<int>> matrix(k, std::vector<int>(l));
-    //Разные при каждом запуске
-    //std::srand(static_cast<unsigned int>(time(nullptr)));
-
-    // Заполняем случайными числами от 1 до 100
     for (int i = 0; i < k; ++i) {
         for (int j = 0; j < l; ++j) {
             matrix[i][j] = std::rand() % 100 + 1;
@@ -18,27 +14,10 @@ std::vector<std::vector<int>> createRandomMatrix(int k, int l) {
 }
 
 
-
-
-int main() {
-
-    const int rows = 1000; 
-    const int cols = 1000; 
-
-    std::vector<std::vector<int>> matrix = createRandomMatrix(rows, cols);
-
-    // Вывод элементов сгенерированной матрицы
-    // for (int i = 0; i < rows; ++i) {
-    //     for (int j = 0; j < cols; ++j) {
-    //         std::cout << matrix[i][j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
+void calculateRowSum(const std::vector<std::vector<int>>& matrix, int rows, int cols) {
 auto row_start = std::chrono::high_resolution_clock::now();
 
     int row_total_sum = 0;
-    // Сумма по строкам
     std::vector<int> row_sum(rows, 0);
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -46,16 +25,20 @@ auto row_start = std::chrono::high_resolution_clock::now();
             row_total_sum += matrix[i][j];
         }
     }
+
 auto row_end = std::chrono::high_resolution_clock::now();
 auto row_duration = std::chrono::duration_cast<std::chrono::milliseconds>(row_end - row_start);
-std::cout <<"Сумма по строкам длительность: " << row_duration.count() << std::endl;
+std::cout <<"Сумма по строкам. длительность: " << row_duration.count() << std::endl;
+
+    std::cout << "\n Общая сумма всех элементов по строкам: " << row_total_sum << std::endl;
+}
 
 
 
+void calculateColSum(const std::vector<std::vector<int>>& matrix, int rows, int cols) {
 auto col_start = std::chrono::high_resolution_clock::now();
 
     int col_total_sum = 0;
-    // Сумма по столбцам
     std::vector<int> col_sum(cols, 0);
     for (int j = 0; j < cols; ++j) {
         for (int i = 0; i < rows; ++i) {
@@ -66,22 +49,22 @@ auto col_start = std::chrono::high_resolution_clock::now();
 
 auto col_end = std::chrono::high_resolution_clock::now();
 auto col_duration = std::chrono::duration_cast<std::chrono::milliseconds>(col_end - col_start);
-std::cout <<"Сумма по столбцам длительность: " <<col_duration.count() << std::endl;
+std::cout <<"Сумма по столбцам. длительность: " << col_duration.count() << std::endl;
 
-    // Вывод результатов
-
-    // std::cout << "Сумма по строкам:\n";
-    // for (int i = 0; i < rows; ++i) {
-    //     std::cout << "Строка " << i+1 << ": " << row_sum[i] << std::endl;
-    // }
-    std::cout << "\n Общая сумма всех элементов по строкам: " << row_total_sum << std::endl;
-
-
-    // std::cout << "\n Сумма по столбцам:\n";
-    // for (int j = 0; j < cols; ++j) {
-    //     std::cout << "Столбец " << j+1 << ": " << col_sum[j] << std::endl;
-    // }
     std::cout << "\n Общая сумма всех элементов по столбцам: " << col_total_sum << std::endl;
+}
+
+
+
+int main() {
     
+    const int rows = 1000; 
+    const int cols = 1000; 
+
+    std::vector<std::vector<int>> matrix = createRandomMatrix(rows, cols);
+
+    calculateRowSum(matrix, rows, cols);
+    calculateColSum(matrix, rows, cols);
+
     return 0;
 }
